05b6000c0e2cb83e3188516fe2b797fbcf9779a1
Sreeja Rao <gsreeja615@gmail.com>
2022-08-01 23:19:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,60 +30,7 @@ from sumy.utils import get_stop_words
 from string import punctuation
 from heapq import nlargest
 
-app = Flask(__name__)
 
-
-@app.route('/summarize/')
-def transcript_fetched_query():
-    # Getting argument from the request
-    video_id = request.args.get('id')  # video_id of the YouTube Video
-    percent = request.args.get('percent')  # percentage of the summary
-    choice = request.args.get('choice')  # summarization choice
-
-    # Checking whether all parameters exist or not
-    if video_id is not None and percent is not None and choice is not None:
-        # every parameter exists here: checking validity of choice
-        choice_list = ["gensim-sum", "spacy-sum", "nltk-sum", "sumy-lsa-sum", "sumy-luhn-sum", "sumy-text-rank-sum"]
-        if choice in choice_list:
-            # Choice Correct Proceeding with Transcript Fetch and its Summarization
-            try:
-                # Using Formatter to store subtitles properly.
-                formatter = TextFormatter()
-                transcript = YouTubeTranscriptApi.get_transcript(video_id)
-                formatted_text = formatter.format_transcript(transcript).replace("\n", " ")
-
-                # Summarizing Formatted Text based upon the request's choice
-                if choice == "gensim-sum":
-                    summary = gensim_summarize(formatted_text, percent)  # Gensim Library for TextRank Based Summary.
-                elif choice == "spacy-sum":
-                    summary = spacy_summarize(formatted_text, percent)  # Spacy Library for frequency-based summary.
-                elif choice == "nltk-sum":
-                    summary = nltk_summarize(formatted_text, percent)  # NLTK Library used for frequency-based summary.
-                elif choice == "sumy-lsa-sum":
-                    summary = sumy_lsa_summarize(formatted_text, percent)  # Sumy for extractive summary using LSA.
-                elif choice == "sumy-luhn-sum":
-                    summary = sumy_luhn_summarize(formatted_text, percent)  # Sumy Library for TF-IDF Based Summary.
-                elif choice == "sumy-text-rank-sum":
-                    summary = sumy_text_rank_summarize(formatted_text, percent)  # Sumy for Text Rank Based Summary.
-                else:
-                    summary = None
-
-                # Returning Result
-                response_list = [
-                    {'fetched_transcript': formatted_text,
-                     'processed_summary': summary,
-                     'length_original': len(formatted_text),
-                     'length_summary': len(summary)}
-                ]
-                return jsonify(success=True,
-                               message="Subtitles for this video was fetched and summarized successfully.",
-                               response=response_list), 200
-
-            # Catching Exceptions
-            except VideoUnavailable:
-                return jsonify(success=False, message="VideoUnavailable: The video is no longer available.",
-                               response=None), 400
-            except TooManyRequests:
                 return jsonify(success=False,
                                message="TooManyRequests: YouTube is receiving too many requests from this IP."
                                        " Wait until the ban on server has been lifted.",
